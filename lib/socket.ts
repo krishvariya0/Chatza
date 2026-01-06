844facc02a916e0d82ce9e0d63780dfed0f085e8
krishvariya0 <variyakrish0@gmail.com>
2026-01-06 18:26:18 +0530

    initial bug

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { logger } from "@/utils/logger";
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:3001";
+const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:3002";
 
 interface ConnectionStats {
     connectedAt?: number;
